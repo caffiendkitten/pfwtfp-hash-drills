d7c2adabaa009611ca5f0c36fde906d6ce6d7e2d
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-05-15 21:15:25 -0700

    Done.

--- lib/hash_drills.rb
+++ lib/hash_drills.rb
@@ -1,3 +1,4 @@
+
 def shopping_list
 {
     :sweets => ["soda", "candy", "potato chips"],
@@ -12,4 +13,48 @@ def shopping_list
 }
 end
 
-# Write methods and implementation here based on README instructions and tests
+def vegetarian_ingredients
+	list = shopping_list
+	list[:protein].delete(:meat)
+	list[:protein][:other].delete("eggs")
+	list
+end
+
+def ketogenic_ingredients
+	list = shopping_list
+	list.delete(:sweets)
+	list[:dairy].delete("milk")
+	list[:dairy].delete("yogurt")
+	list.delete(:fruits)
+	list.delete(:grains)
+	list
+end
+
+def mediterranean_ingredients
+	list = shopping_list
+	list.delete(:sweets)	
+	list[:protein][:meat].delete("steak")
+	list[:protein][:other].delete("beans")
+	list[:protein][:other].delete("eggs")
+	list
+end
+
+
+
+
+def vegan_ingredients
+	list = shopping_list
+	list[:protein].delete(:meat)
+	list[:protein][:other].delete("eggs")
+	list.delete(:dairy)	
+
+	list
+end
+
+def remove_allergens(allergen)
+	allergen_list = allergen
+	# list[:protein].delete(:meat)
+	allergen_list[:protein][:other].delete("nuts")
+	allergen_list
+	# binding.pry
+end
